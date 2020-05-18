3ad5946e046466b23162075cc0c5ff99c8c683e2
gregoire hornung <greghor4@gmail.com>
2020-05-18 15:27:56 +0200

    generate rst out of md

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="study univariate relations",
+    description="Visualize the relation between a dependent variable and a predictor in a meaningful way",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/greghor/tprojection',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
