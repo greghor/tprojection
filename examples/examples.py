ce8d01384148433799ffcb3fe6e05332df23bb39
gregoire hornung <greghor4@gmail.com>
2020-05-18 14:48:24 +0200

    update examples

--- examples/examples.py
+++ examples/examples.py
@@ -14,19 +14,19 @@ target = "survived"
 
 
 ##{ example of target and feature being both categorical. The confidence interval is estiamted by bootsrapping with 100 estimators
-tproj = Tprojection(df, target, feature, target_type="categorical", feature_type="categorical", n_estimators=100)
+tproj = Tprojection(df, target, "sex", target_type="categorical", feature_type="categorical", n_estimators=100)
 tproj.plot()
 ##}
 
 ##{ example of target and feature being  categorical and continuous, resp.
 ## note that I do not explicitely provide the type of variables here (categorical or continuous),
 ## it is infered by the class using a simple rule of thumb
-tproj = Tprojection(df, target, feature)
+tproj = Tprojection(df, target, "fare")
 tproj.plot()
 ##}
 
 ##{ example of target and feature being both categorical. Here, we use a dummy encoding since the number of original features modalities is too high
-tproj = Tprojection(df, target, feature, feature_type="categorical", nb_modalities=10, n_estimators=100)
+tproj = Tprojection(df, target, "cabin", feature_type="categorical", nb_modalities=10, n_estimators=100)
 tproj.plot()
 print(tproj.encoding) # contains the mapping between the original modalities and the encoded modalities
 ##}
